c6c50eee5b42a883dacb67b69daa80065c51e8d8
abdo_nashat <146928703+abdelrahman72-22@users.noreply.github.com>
2025-03-02 15:13:31 +0200

    Add files via upload

---
+++ bruteforce Monoalphabetic.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  2 14:49:39 2025
+
+@author: Lenovo
+"""
+
+from itertools import permutations
+import string
+
+# English letter frequency (approximate order)
+FREQUENCY_ORDER = "ETAOINSHRDLCUMWFGYPBVKJXQZ"
+
+# Function to decrypt text using a given key mapping
+def decrypt(text, key_map):
+    return "".join(key_map.get(char, char) for char in text)
+
+# Brute-force function to try different key mappings
+def brute_force_monoalphabetic(ciphertext):
+    ciphertext = ciphertext.upper()
+    unique_chars = sorted(set(ciphertext) & set(string.ascii_uppercase))
+    
+    # Generate possible mappings using letter frequency analysis
+    for perm in permutations(FREQUENCY_ORDER[:len(unique_chars)], len(unique_chars)):
+        key_map = dict(zip(unique_chars, perm))
+        decrypted_text = decrypt(ciphertext, key_map)
+        print(f"Trying key: {key_map}\nDecrypted text: {decrypted_text}\n")
+
+# User interaction
+if __name__ == "__main__":
+    cipher_text = input("Enter the encrypted text: ")
+    brute_force_monoalphabetic(cipher_text)
